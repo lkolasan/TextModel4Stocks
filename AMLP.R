506985c0fe664f371ba0fcd665679a11f0666446
Lakshman <lkolasan@uncc.edu>
2017-05-02 01:50:29 -0400

    Update AMLP.R

--- AMLP.R
+++ AMLP.R
@@ -28,58 +28,13 @@ cndjia$all <- gsub("([<>])|[[:punct:]]", "\\1", cndjia$all)
 # Keeping the required columns and deleting rest 
 cndjia <- cndjia[, c('Date', 'Label', 'all')]
 
-# exclude stopwords and headline tokens
-control <- list(removeNumbers = TRUE, tolower = TRUE, 
-                stopwords = c(stopwords(kind = 'SMART'), '<s>'))
-
-dtm <- Corpus(VectorSource(cndjia$all)) %>% 
-  DocumentTermMatrix(control=control)
-
-split_index <- cndjia$Date <= '2014-12-31'
-
-ytrain <- as.factor(cndjia$Label[split_index])
-xtrain <- Matrix(as.matrix(dtm)[split_index, ], sparse=TRUE)
-
-ytest <- as.factor(cndjia$Label[!split_index])
-xtest <- Matrix(as.matrix(dtm)[!split_index, ], sparse=TRUE)
-
-# Train the model
-glmnet.fit <- cv.glmnet(x=xtrain, y=ytrain, family='binomial', alpha=0)
-
-# Generate predictions
-preds <- predict(glmnet.fit, newx=xtest, type='response', s='lambda.min')
-
-# Put results into dataframe for plotting.
-results <- data.frame(pred=preds, actual=ytest)
-
-ggplot(results, aes(x=preds, color=actual)) + geom_density()
-
-prediction <- prediction(preds, ytest)
-perf <- performance(prediction, measure = "tpr", x.measure = "fpr")
-
-auc <- performance(prediction, measure = "auc")
-auc <- auc@y.values[[1]]
-auc
-
-roc.data <- data.frame(fpr=unlist(perf@x.values),
-tpr=unlist(perf@y.values))
-
-ggplot(roc.data, aes(x=fpr, ymin=0, ymax=tpr)) +
-geom_ribbon(alpha=0.2) +
-geom_line(aes(y=tpr)) +
-geom_abline(slope=1, intercept=0, linetype='dashed') +
-ggtitle("ROC Curve") +
-ylab('True Positive Rate') +
-xlab('False Positive Rate')
-
 # Necessary to set this option on Linux machines, otherwise the NGrameTokenizer will cause our 
 # DocumentTermMatrix call to hang. 
 options(mc.cores=1)
 
 BigramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 2, max = 2))
 
-control <- list(tokenize=BigramTokenizer,
-bounds = list(global = c(20, 500)))
+control <- list(tokenize=BigramTokenizer,stopwords = c(stopwords(kind = 'SMART'),bounds = list(global = c(20, 500))))
 
 dtm <- Corpus(VectorSource(cndjia$all)) %>%
 tm_map(removeNumbers) %>%
@@ -94,35 +49,6 @@ xtrain <- Matrix(as.matrix(dtm)[split_index, ], sparse=TRUE)
 
 ytest <- as.factor(cndjia$Label[!split_index])
 xtest <- Matrix(as.matrix(dtm)[!split_index, ], sparse=TRUE)
-
-# Train the model
-glmnet.fit <- cv.glmnet(x=xtrain, y=ytrain, family='binomial', alpha=0)
-
-# Generate predictions
-preds <- predict(glmnet.fit, newx=xtest, type='response', s="lambda.min")
-
-# Put results into dataframe for plotting.
-results <- data.frame(pred=preds, actual=ytest)
-
-ggplot(results, aes(x=preds, color=actual)) + geom_density()
-
-prediction <- prediction(preds, ytest)
-perf <- performance(prediction, measure = "tpr", x.measure = "fpr")
-
-auc <- performance(prediction, measure = "auc")
-auc <- auc@y.values[[1]]
-auc
-
-roc.data <- data.frame(fpr=unlist(perf@x.values),
-tpr=unlist(perf@y.values))
-
-ggplot(roc.data, aes(x=fpr, ymin=0, ymax=tpr)) +
-geom_ribbon(alpha=0.2) +
-geom_line(aes(y=tpr)) +
-geom_abline(slope=1, intercept=0, linetype='dashed') +
-ggtitle("ROC Curve") +
-ylab('True Positive Rate') +
-xlab('False Positive Rate')
 
 library(randomForest)
 set.seed(32) 
@@ -146,10 +72,8 @@ auc <- performance(pred, measure = "auc")
 auc <- auc@y.values[[1]]
 auc
 
+roc.data <- data.frame(fpr=unlist(perf@x.values), tpr=unlist(perf@y.values), model="RF")
 
-roc.data <- data.frame(fpr=unlist(perf@x.values),
-                       tpr=unlist(perf@y.values),
-                       model="RF")
 ggplot(roc.data, aes(x=fpr, ymin=0, ymax=tpr)) +
   geom_ribbon(alpha=0.2) +
   geom_line(aes(y=tpr)) +
